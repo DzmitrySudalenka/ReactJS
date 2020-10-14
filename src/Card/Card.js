549c1024b24ca1d3f957eb32518b8af8d299e025
DzmitrySudalenka <sudalenko@gmail.com>
2020-10-14 19:49:41 +0300

    Task 1 solution

---
+++ src/Card/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Card.css';
+
+const card = ( props ) => {
+  return (
+    <div className="card">
+      <h3 className="card-title">{props.caption}</h3>
+      <hr className="card-sep"/>
+      <p className="card-text">{props.children}</p>
+    </div>
+  )
+};
+
+export default card;
