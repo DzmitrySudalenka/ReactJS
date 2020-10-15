aa26670a680c7919e0426d2b927c1f3b6c712aee
DzmitrySudalenka <sudalenko@gmail.com>
2020-10-15 15:43:14 +0300

    Task 2 solution

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Card.css';
 
 const Сard = ( props ) => {
+
+  const [cardState, setCardState] = useState({
+    isStyleChanged: false
+  });
+
+  const changeStyleHandler = (event) => {
+    setCardState({
+      isStyleChanged: event.target.checked
+    });
+  };
+
   return (
-    <div className="card">
-      <h3 className="card-title">{props.caption}</h3>
+    <div className={`card ${cardState.isStyleChanged ? 'dark' : ''}`}>
+      <div className="card-title-wrap">
+        <h3 className="card-title">{props.caption}</h3>
+        <input type="checkbox" onChange={changeStyleHandler}/>
+      </div>
       <hr className="card-sep"/>
       <p className="card-text">{props.children}</p>
     </div>
